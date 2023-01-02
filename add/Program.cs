a93f761ddf84cd6820df6fc20e7e549ef0f8e1d7
udach1n <udachin23@gmail.com>
2023-01-02 13:21:21 +0100

    full project

---
+++ add/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Front;
+
+namespace add
+{
+    public  class Program
+    {
+        static public void Main()
+        {
+            Front.Appearance.MakeLibrary("Artem");
+            Front.Appearance.Menu();
+        }
+    }
+}
